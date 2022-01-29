c1ed6aad350ac1f98de35e4e4b8567ede67d189d
Ahmad Al Monkez <98638436+Ahmad-Al-Monkez@users.noreply.github.com>
2022-01-29 10:32:51 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/*
+ *    The Main Function.
+ */
+
+#include "BOH.hpp"  // BOH Header, This Header Will Contain All Required Headers, Function Prototypes And Global Data.
+
+int main(int argc, char** argv){
+       InitializeProgram(argc, argv); // Setting Up Program, Parsing Argument And So On..
+
+  
+}
